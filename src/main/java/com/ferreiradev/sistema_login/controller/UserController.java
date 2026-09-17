1939a9404dd55066d98a80d33e13f1ad59f1dfc2
jamillyferreira <jamillyferreira039@gmail.com>
2026-09-16 20:21:48 -0400

    refactor: muda o endpoint para consultar usuário autenticado

--- src/main/java/com/ferreiradev/sistema_login/controller/UserController.java
+++ src/main/java/com/ferreiradev/sistema_login/controller/UserController.java
@@ -1,34 +1,24 @@
-package com.ferreiradev.sistema_login.controller;
+    package com.ferreiradev.sistema_login.controller;
 
-import com.ferreiradev.sistema_login.dtos.request.LoginRequestDTO;
-import com.ferreiradev.sistema_login.dtos.request.RegisterRequestDTO;
-import com.ferreiradev.sistema_login.dtos.response.RegisterResponseDTO;
-import com.ferreiradev.sistema_login.service.UserService;
-import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+    import com.ferreiradev.sistema_login.dtos.response.RegisterResponseDTO;
+    import com.ferreiradev.sistema_login.mapper.AuthMapper;
+    import com.ferreiradev.sistema_login.model.User;
+    import lombok.RequiredArgsConstructor;
+    import org.springframework.http.ResponseEntity;
+    import org.springframework.security.core.annotation.AuthenticationPrincipal;
+    import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/auth")
-public class UserController {
 
-    private final UserService userService;
+    @RestController
+    @RequiredArgsConstructor
+    @RequestMapping("/users")
+    public class UserController {
 
-    public UserController(UserService userService) {
-        this.userService = userService;
+        private final AuthMapper authMapper;
+
+        @GetMapping("/me") // Acessivel somente para usuario logado
+        public ResponseEntity<RegisterResponseDTO> me(@AuthenticationPrincipal User user) {
+            return ResponseEntity.ok(authMapper.toRegisterResponseDTO(user));
+        }
+
     }
-
-    @PostMapping("/register")
-    public ResponseEntity<RegisterResponseDTO> register(@RequestBody @Valid RegisterRequestDTO request) {
-        RegisterResponseDTO response = userService.registerUser(request);
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
-    }
-
-    @PostMapping("/login")
-    public ResponseEntity<RegisterResponseDTO> login(@RequestBody @Valid LoginRequestDTO request) {
-        RegisterResponseDTO response = userService.loginUser(request);
-        return ResponseEntity.status(HttpStatus.OK).body(response);
-    }
-
-}
