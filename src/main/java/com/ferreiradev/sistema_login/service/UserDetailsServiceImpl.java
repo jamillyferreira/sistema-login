5eadadc7d57081cdd8785b29814f12d365693811
jamillyferreira <jamillyferreira039@gmail.com>
2026-09-16 20:14:37 -0400

    feat: adiciona serviço para buscar usuário por email

---
+++ src/main/java/com/ferreiradev/sistema_login/service/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.ferreiradev.sistema_login.service;
+
+import com.ferreiradev.sistema_login.repository.UserRepository;
+import org.jspecify.annotations.NonNull;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(@NonNull String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+    }
+}
