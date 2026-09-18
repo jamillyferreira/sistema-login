279ed34f712146f5d45467364169b8cededd7676
jamillyferreira <jamillyferreira039@gmail.com>
2026-09-18 08:29:19 -0400

    refactor: adiciona log de user autenticado

--- src/main/java/com/ferreiradev/sistema_login/controller/UserController.java
+++ src/main/java/com/ferreiradev/sistema_login/controller/UserController.java
@@ -1,24 +1,28 @@
-    package com.ferreiradev.sistema_login.controller;
+package com.ferreiradev.sistema_login.controller;
 
-    import com.ferreiradev.sistema_login.dtos.response.UserResponse;
-    import com.ferreiradev.sistema_login.mapper.AuthMapper;
-    import com.ferreiradev.sistema_login.model.User;
-    import lombok.RequiredArgsConstructor;
-    import org.springframework.http.ResponseEntity;
-    import org.springframework.security.core.annotation.AuthenticationPrincipal;
-    import org.springframework.web.bind.annotation.*;
+import com.ferreiradev.sistema_login.dtos.response.UserResponse;
+import com.ferreiradev.sistema_login.mapper.AuthMapper;
+import com.ferreiradev.sistema_login.model.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 
-    @RestController
-    @RequiredArgsConstructor
-    @RequestMapping("/users")
-    public class UserController {
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
+public class UserController {
 
-        private final AuthMapper authMapper;
+    private final AuthMapper authMapper;
 
-        @GetMapping("/me") // Acessivel somente para usuario logado
-        public ResponseEntity<UserResponse> me(@AuthenticationPrincipal User user) {
-            return ResponseEntity.ok(authMapper.toRegisterResponseDTO(user));
-        }
+    @GetMapping("/me") // Acessivel somente para usuario logado
+    public ResponseEntity<UserResponse> me(@AuthenticationPrincipal User user) {
+        log.info("Consulta de usuário autenticado: id={}, name={}", user.getId(), user.getName());
 
+        return ResponseEntity.ok(authMapper.toRegisterResponseDTO(user));
     }
+
+}
